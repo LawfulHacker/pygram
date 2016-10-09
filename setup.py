ea8cabd8af1edb7da7a26166d66421de2f11f11b
Moacir Moda <moa.moda@gmail.com>
2016-10-09 19:31:01 -0300

    making instalable package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pygram',
+      version='0.1',
+      description='Python unofficial API to connect with Instagram web',
+      url='https://github.com/moacirmoda/pygram',
+      author='Moacir Moda',
+      author_email='moa.moda@gmail.com',
+      license='GNU',
+      packages=['pygram'],
+      zip_safe=False)
